7f53b628cbc9a662c64440b40f7cddd9ce60afee
JoaoMota <joaoefmota@gmail.com>
2022-09-29 14:18:47 +0100

    "First commit"

---
+++ script.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+const log = console.log;
+log(chalk.greenBright.bgRedBright.overline('Hello Diogo!'));
+log(chalk.yellow.bgBlue.underline('Hello Iryna'));
+log(chalk.red.bgWhite.italic('Hello Isamu!'));
+log(chalk.yellow.bgBlackBright.bold('Hello Elif!'));
